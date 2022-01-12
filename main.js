16568b6fa55dbed0359f47c598ffa5d4870fb4af
Justin Xu <justin.zhenchen.xu@gmail.com>
2022-01-11 23:59:14 -0800

    allows drawing by clicking, and reset canvas

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
+
 const canvas=document.querySelector(".canvas");
-const cell=document.querySelectorAll(".cell");
-
-/*
-function drawGrid(size){
-    for (let i=0;i<size;i++){
-        var row=document.createElement("div");
-        row.style.display="flex";
-        canvas.appendChild(row);
-        canvas.style.flexDirection="column";
-        for(let n=0; n<size; n++){
-            var cell=document.createElement("div");
-            cell.classList.add("cell");
-            row.appendChild(cell);
-            row.style.flexDirection="row";
-        }
-    }
-}
-
-drawGrid(10);
-*/
+const btn=document.querySelector(".reset");
 
 function drawGrid(x, y){
     for (let i=0;i<y;i++){
@@ -36,59 +18,36 @@ function drawGrid(x, y){
     }
 }
 
-drawGrid(12, 12);
+drawGrid(20, 20);
 
-//var cell=document.querySelectorAll(".cell");
-
-/*
-function fillCell(){
-    console.log(document.getElementsByClassName("cell"));
-    document.getElementsByClassName("cell").style.color="black";
-    
-}
-
-fillCell();
-*/
-
-console.log(cell);
-
-
-/*
-function drawRow(width){
-    for (let i=0; i<width; i++){
-        var childCell=document.createElement("div");
-        childCell.classList.add("cell");
-        cell.appendChild(childCell);
+function fillBlack(e){
+    let name=e.target.className;
+    if (name == "cell"){
+        e.target.style.backgroundColor="black";
     }
 }
 
-drawRow(10);
-*/
-
-
-
-/*
-function drawGrid(width, height){
-    for (let i=0; i<height; i++){
-        drawColumn(width);
+function clear(){
+    var box=document.querySelectorAll(".cell");
+    let length=box.length;
+    for (i=0; i<length; i++){
+        box[i].style.backgroundColor="white";
     }
 }
 
-drawGrid(10, 10);
-*/
-
-
+btn.addEventListener('click', clear);
+canvas.addEventListener('mouseup', fillBlack);
 
 /*
-function drawCell(location){
-    location.classList.add("cell");
-    const cell=document.querySelector(".cell");
-    cell.style.height="10px";
-    cell.style.width="10px";
-    cell.style.border= "1px solid black";
-    console.log("success");
-}
+var box=document.querySelectorAll(".cell");
+//this gives array
+console.log(box);
 
-//drawCell(canvas);
+//this gets very first cell
+var sample=box[0];
+console.log(sample);
+
+//this gives total amount of cells
+let x=box.length;
+console.log(x);
 */
-
